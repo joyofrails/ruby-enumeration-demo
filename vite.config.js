f914a531ebd37113af542ff5b06e18935a28785b
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-18 10:31:56 -0500

    Add github deploy workflow
    From Vite docs https://vite.dev/guide/static-deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [react()],
+  base: '/demo-ruby-enumeration/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
 });
