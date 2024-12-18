e297b8551c4a92af6ddbf4e716faa9199ed93524
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-18 11:18:26 -0500

    Fix repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/demo-ruby-enumeration/',
+  base: '/ruby-enumeration-demo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
